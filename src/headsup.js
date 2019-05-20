55d1658ae7b1aa5ba5fc2b941ea0f9a531647898
Caleb Eby <caleb.eby01@gmail.com>
2019-05-20 15:34:50 -0700

    Add prettier

--- src/headsup.js
+++ src/headsup.js
@@ -6,81 +6,81 @@ export default ({
   debounce = false,
   stickyBodyClass = 'header-is-stuck'
 } = {}) => {
-  let show = true                                         // initial boolean value
-  let prev = window.pageYOffset                           // initial window position
+  let show = true; // initial boolean value
+  let prev = window.pageYOffset; // initial window position
 
-  const header = document.querySelector(selector)
-  const styles = window.getComputedStyle(header)
+  const header = document.querySelector(selector);
+  const styles = window.getComputedStyle(header);
 
-  const body = document.body
+  const body = document.body;
 
-  const headerHeight = () => {                            // computes total height of the element
-    const widthAndPadding = header
-      .getBoundingClientRect()
-      .height
-    const marginTop = parseFloat(styles['margin-top'])
-    const marginBot = parseFloat(styles['margin-bottom'])
+  const headerHeight = () => {
+    // computes total height of the element
+    const widthAndPadding = header.getBoundingClientRect().height;
+    const marginTop = parseFloat(styles['margin-top']);
+    const marginBot = parseFloat(styles['margin-bottom']);
 
-    return widthAndPadding + marginTop + marginBot
-  }
+    return widthAndPadding + marginTop + marginBot;
+  };
 
-  const fixedShow = () => {                               // shows the element
-    header
-      .style
-      .top = '0'
+  const fixedShow = () => {
+    // shows the element
+    header.style.top = '0';
 
-    body
-      .classList.add(stickyBodyClass)
+    body.classList.add(stickyBodyClass);
 
-    show = true
-  }
+    show = true;
+  };
 
-  const fixedHide = () => {                               // hides the element
-    header
-      .style
-      .top = `-${headerHeight()}px`
+  const fixedHide = () => {
+    // hides the element
+    header.style.top = `-${headerHeight()}px`;
 
-      body
-        .classList.remove(stickyBodyClass)
+    body.classList.remove(stickyBodyClass);
 
-    show = false
-  }
+    show = false;
+  };
 
-  const onScrollFunction = _ => {                         // performs logic on each scroll event
-    const current = window.pageYOffset
+  const onScrollFunction = _ => {
+    // performs logic on each scroll event
+    const current = window.pageYOffset;
 
-    current > prev && current >= (headerHeight() / 2)
-      ? show ? fixedHide() : null
-      : show ? null : fixedShow()
+    current > prev && current >= headerHeight() / 2
+      ? show
+        ? fixedHide()
+        : null
+      : show
+      ? null
+      : fixedShow();
 
-    prev = current
-  }
+    prev = current;
+  };
 
-  const debounceFunc = wait => {                          // debouncing function
-    if (!wait) return onScrollFunction
+  const debounceFunc = wait => {
+    // debouncing function
+    if (!wait) return onScrollFunction;
 
-    let timeout = null
+    let timeout = null;
     return () => {
       if (!timeout) {
         timeout = setTimeout(() => {
-          onScrollFunction()
-          timeout = null
-        }, wait)
+          onScrollFunction();
+          timeout = null;
+        }, wait);
       }
-    }
-  }
+    };
+  };
 
-  body
-    .style['margin-top'] = `${headerHeight()}px` // adjust body margin to make space for header
+  body.style['margin-top'] = `${headerHeight()}px`; // adjust body margin to make space for header
 
-  body
-    .classList.add(stickyBodyClass)
+  body.classList.add(stickyBodyClass);
 
-  Object.assign(header.style, {                           // assign fixed position and transition to header
+  Object.assign(header.style, {
+    // assign fixed position and transition to header
     position: 'fixed',
     top: '0',
     transition: `top ${duration}s ${easing} ${delay}s`
-  })
+  });
 
-  window.addEventListener('scroll', debounceFunc(debounce))
-}
+  window.addEventListener('scroll', debounceFunc(debounce));
+};
